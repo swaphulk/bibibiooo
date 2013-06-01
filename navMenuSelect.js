97fc2323fef1b14d92b2d93f6d3307d2e3b61e61
stuart <incinerision@gmail.com>
2013-06-01 15:24:44 -0500

    menu links switch out main screen

---
+++ navMenuSelect.js
@@ -0,0 +1,29 @@
+$( document ).ready(function() {
+    
+  $('.menuLink').click(function(event) {
+    event.preventDefault();
+    if (!event.target) {
+      return;
+    }
+    switch (event.target.id) {
+      case 'introScreenLink':
+        $('.mainDiv').hide();
+        $('#introScreenDiv').show();
+        break;
+      case 'mainMenuLink':
+        $('.mainDiv').hide();
+        $('#mainGameDiv').show();
+        break;
+      case 'helpMenuLink':
+        $('.mainDiv').hide();
+        $('#helpDiv').show();
+        break;
+      case 'achievementsMenuLink':
+        $('.mainDiv').hide();
+        $('#acheivementsDiv').show();
+        break;
+    }
+    return;
+  });
+
+});
