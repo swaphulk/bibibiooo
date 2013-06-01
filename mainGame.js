882d551f0d7c584e66fcffd15abcb30fbfd5da7a
stuart <incinerision@gmail.com>
2013-06-01 17:20:10 -0500

    restructure house and family objects

--- mainGame.js
+++ mainGame.js
@@ -4,28 +4,32 @@ var getFamily = function() {
   var family = {}; 
   family.members = [];
 
-  family.members[0] = {name: "dad", age: "adult", fear: 100, relationships: [null, 100, 100, 100]};
-  family.members[1] = {name: "mom", age: "adult", fear: 100, relationships: [100, null, 100, 100]};
-  family.members[2] = {name: "boy", age: "teen", fear: 100, relationships: [100, 100, null, 100]};
-  family.members[3] = {name: "girl", age: "child", fear: 100, relationships: [100, 100, 100, null]};
+  family.members[0] = {name: "dad", age: "adult", fear: 100};
+  family.members[1] = {name: "mom", age: "adult", fear: 100};
+  family.members[2] = {name: "boy", age: "teen", fear: 100};
+  family.members[3] = {name: "girl", age: "child", fear: 100};
+
+  family.relationships = [
+    [null, 100,  100,  100],
+    [null, null, 100,  100],
+    [null, null, null, 100]
+  ];
 
   family.lastName = "Testers";
 
   return family;
 }
 
-
-
 var getHouse = function() {
   var house = {};
 
   house.rooms = [];
-  house.rooms[0] = {name: "Living Room", couch: true, chair: true, chandelier: true, rug: true, fireplace: true, plant: true, people: 0};
-  house.rooms[1] = {name: "Bathroom", toilet: true, sink: true, bathtub: true, lamp: true, mirror: true, people: 0};
-  house.rooms[2] = {name: "Kitchen", sink: true, oven: true, refrigerator: true, toaster: true, people: 0};
-  house.rooms[3] = {name: "Master Bedroom", bed: true, dresser: true, fireplace: true, closet: true, mirror: true, people: 0};
-  house.rooms[4] = {name: "Second Bedroom", bed: true, dresser: true, desk: true, people: 0};
-  house.rooms[5] = {name: "Conservatory", piano: true, chair: true, harp: true, plant: true, people: 0};
+  house.rooms[0] = {name: "Living Room", contents: {'people': [], 'objects': ['couch', 'chair', 'chandelier', 'rug', 'fireplace', 'plant']}};
+  house.rooms[1] = {name: "Bathroom", contents: {'people': [], 'objects': ['toilet', 'sink', 'bathtub', 'lamp', 'mirror']}};
+  house.rooms[2] = {name: "Kitchen", contents: {'people': [], 'objects': ['sink', 'oven', 'refrigerator', 'toaster']}};
+  house.rooms[3] = {name: "Master Bedroom", contents: {'people': [], 'objects': ['bed', 'dresser', 'fireplace', 'closet']}}; 
+  house.rooms[4] = {name: "Second Bedroom", contents: {'people': [], 'objects': ['bed', 'dresser', 'desk']}};
+  house.rooms[5] = {name: "Conservatory", contents: {'people': [], 'objects': ['piano', 'chair', 'harp', 'plant']}};
 
   return house;
 }
@@ -52,8 +56,9 @@ timeControl.nextDay = function() {  // called at the end of a day (after attacks
 
 
 
-family = getFamily();
-house = getHouse();
+var family = {};
+var house = getHouse();
 timeControl.newMonth();
+
 
 });
